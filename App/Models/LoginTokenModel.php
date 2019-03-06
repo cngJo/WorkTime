f465ec407f75b0c00baa56849a29324363767e43
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-06 16:30:16 +0100

    added the LoginTokenModel, which can read and write login_tokens from the database

--- App/Models/LoginTokenModel.php
+++ App/Models/LoginTokenModel.php
@@ -13,5 +13,55 @@ use DB\SQL\Mapper;
 
 class LoginTokenModel extends Mapper
 {
+    public function __construct()
+    {
+        /** @var \Base $f3 */
+        $f3 = \Base::instance();
 
+        parent::__construct($f3->get('DB'), 'login_tokens');
+    }
+
+    /**
+     * Inserts an Token into the database
+     *
+     * @param $token
+     * @param $user_id
+     */
+    public function insertToken($token, $user_id) {
+        $this->token = $token;
+        $this->user_id = $user_id;
+        $this->save();
+    }
+
+    /**
+     * Removes the given token from the database
+     *
+     * @param $token
+     */
+    public function deleteToken($token) {
+        $this->erase(['token=?', $token]);
+    }
+
+    /**
+     * Removes all tokens from the user with the given id
+     *
+     * @param $user_id
+     */
+    public function deleteUserToken($user_id) {
+        $this->erase(['user_id=?', $user_id]);
+    }
+
+    /**
+     * Returns an array of all tokens, assigned to the user with the given id
+     *
+     * @param $user_id
+     * @return array
+     */
+    public function getUserTokens($user_id) {
+        $tokens = [];
+        foreach ($this->find(['user_id=?', $user_id]) as $token) {
+            array_push($tokens, $token->token);
+        }
+        return $tokens;
+    }
 }

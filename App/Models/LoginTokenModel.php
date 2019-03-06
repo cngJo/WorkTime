667edd81b8b04cf6636cf17536a7f0f9d9e263ee
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-06 16:08:22 +0100

    added UserModel and LoginTokenModel for handling user login

---
+++ App/Models/LoginTokenModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LocalAdmin
+ * Date: 06.03.2019
+ * Time: 16:07
+ */
+
+namespace App\Models;
+
+
+use DB\SQL\Mapper;
+
+class LoginTokenModel extends Mapper
+{
+
+}
---
+++ App/Models/UserModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LocalAdmin
+ * Date: 06.03.2019
+ * Time: 16:07
+ */
+
+namespace App\Models;
+
+
+use DB\SQL\Mapper;
+
+class UserModel extends Mapper
+{
+
+}

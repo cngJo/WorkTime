0aa23cb1bccd0c15e81b2fd7d19088812a1922df
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-07 09:26:16 +0100

    updated database

--- db.sql
+++ db.sql
@@ -16,6 +16,17 @@
 CREATE DATABASE IF NOT EXISTS `worktimes` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `worktimes`;
 
+-- Exportiere Struktur von Tabelle worktimes.login_tokens
+CREATE TABLE IF NOT EXISTS `login_tokens` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `token` char(64) NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_login_tokens_users` (`user_id`),
+  CONSTRAINT `FK_login_tokens_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle worktimes.times
 CREATE TABLE IF NOT EXISTS `times` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -25,6 +36,17 @@ CREATE TABLE IF NOT EXISTS `times` (
   `notes` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Exportiere Struktur von Tabelle worktimes.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `email` text NOT NULL,
+  `role` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Daten Export vom Benutzer nicht ausgewählt
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;

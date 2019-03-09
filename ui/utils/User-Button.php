e09afd13ebcce29332be9192eb0d30e59730fbe6
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-09 21:35:16 +0100

    fixed the register link in the user-button dropdown

--- ui/utils/User-Button.php
+++ ui/utils/User-Button.php
@@ -2,7 +2,7 @@
     <button class="btn btn-dropdown">{{ @user.name }}</button>
     <div id="user-dropdown">
         <ul>
-            <li><a href="registser">{{ @user.register }}</a></li>
+            <li><a href="register">{{ @user.register }}</a></li>
             <li><a href="login">{{ @user.login }}</a></li>
             <li><a href="logout">{{ @user.logout }}</a></li>
         </ul>

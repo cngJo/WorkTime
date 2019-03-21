59b63c0b127f126ec2b8c0791424032207b0f1af
Johannes Przymusinski <johannes@przymusinski.de>
2019-03-21 15:09:54 +0100

    fixed bug where the header gets renders with the wrong options when worktime gets installed

--- ui/utils/header.php
+++ ui/utils/header.php
@@ -2,7 +2,7 @@
     <nav>
         <div class="clearfix">
             <div class="float-left"><form action="home" method="get" id="home-form"><a onclick="document.querySelector('#home-form').submit()">WorkTime</a></form></div>
-            <check if="{{ @SESSION.loggedin != 'false' }}">
+            <check if="{{ @SESSION.loggedin == 'true' }}">
                 <true>
                     <div class="float-left header-navigation-item"><a href="profile">{{ @header.profile }}</a></div>
                     <div class="float-left header-navigation-item"><a href="logout">{{ @header.logout }}</a></div>

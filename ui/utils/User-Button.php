d7982ba7ebc9062155ff2f4b1bda5b2b57831eac
Johannes Przymusinski <johannes@przymusinski.de>
2019-04-09 07:49:02 +0200

    UserButton now shows always the right options
    There was a bug, where the UserButton showed links to the profile and to the logout page, when you were not logged in.

--- ui/utils/User-Button.php
+++ ui/utils/User-Button.php
@@ -2,7 +2,7 @@
     <button class="btn btn-dropdown">{{ @user.name }}</button>
     <div id="user-dropdown">
         <ul>
-            <check if="{{ @SESSION.loggedin != 'false' }}">
+            <check if="{{ @SESSION.loggedin == 'true' }}">
                 <true>
                     <li><a href="profile">{{ @header.profile }}</a></li>
                     <li><a href="logout">{{ @header.logout }}</a></li>

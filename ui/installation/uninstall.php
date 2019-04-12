76ecf9b2121b55c67a017eb633d2e0674f62feb2
Johannes Przymusinski <johannes@przymusinski.de>
2019-04-12 14:54:20 +0200

    updated uninstall interface and added missing translation
    the uninstaller now renders the effects as paragraphs instad of an unordered list
    added missing en_US translation for the export feature in the profile

--- ui/installation/uninstall.php
+++ ui/installation/uninstall.php
@@ -1,27 +1,29 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <include href="utils/head.php" />
+    <link rel="stylesheet" href="ui/css/install.css">
 </head>
+
 <body>
 
-<div class="content">
-    <div class="row">
-        <div class="column column-50 column-offset-25 div-center">
-            <h1>{{ @uninstall.title }}</h1>
-            <form action="uninstall" method="post">
-                <h2 class="color-red">{{ @uninstall.warning }}</h2>
-                <ul>
+    <div class="content">
+        <div class="row">
+            <div class="column column-50 column-offset-25 div-center">
+                <h1>{{ @uninstall.title }}</h1>
+                <form action="uninstall" method="post">
+                    <h2 class="color-red">{{ @uninstall.warning }}</h2>
                     <repeat group="{{ @uninstall.effects }}" value="{{ @effect }}">
-                    <li>{{ @effect }}</li>
+                        <p>{{ @effect }}</p>
                     </repeat>
-                </ul>
-                <input type="checkbox" id="sure" name="sure" required/><span>{{ @uninstall.checkbox }}</span>
-                <input type="submit" value="{{ @uninstall.submit }}">
-            </form>
+                    <input type="checkbox" id="sure" name="sure" required /><span>{{ @uninstall.checkbox }}</span>
+                    <input type="submit" value="{{ @uninstall.submit }}">
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 </body>
+
 </html>
